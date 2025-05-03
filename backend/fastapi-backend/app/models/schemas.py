861bccc39234b65dc911e011960d116d74828994
y1220 <ga.2.y1220@gmail.com>
2025-05-03 16:31:27 +0200

    remove unnecessary schemas

--- backend/fastapi-backend/app/models/schemas.py
+++ backend/fastapi-backend/app/models/schemas.py
@@ -2,34 +2,6 @@ from pydantic import BaseModel, Field
 from typing import List, Optional
 from datetime import datetime
 
-class ItemBase(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
-class ItemCreate(ItemBase):
-    pass
-
-class Item(ItemBase):
-    id: int
-
-    class Config:
-        orm_mode = True
-
-class UserBase(BaseModel):
-    username: str
-    email: str
-
-class UserCreate(UserBase):
-    password: str
-
-class User(UserBase):
-    id: int
-
-    class Config:
-        orm_mode = True
-
 class Message(BaseModel):
     message: str
 
